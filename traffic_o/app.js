ebbdcbd683d5b6bbfe4c26497ccc30054c4c42d7
Reshav Rajput <reshavkumar_15206@aitpune.edu.in>
2017-09-14 01:57:47 +0530

    Update app.js

--- traffic_o/app.js
+++ traffic_o/app.js
@@ -3,16 +3,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
-
 var firebase = require("firebase");
-var Promises = require("bluebird")
+var Promises = require("bluebird");
+var fs = require('fs');
+var https = require('https');
+
+var options = {
+ key: fs.readFileSync('key.pem'),
+ cert: fs.readFileSync('cert.pem')
+};
 
 var recv_data;
 var config = {
- apiKey: "AIzaSyAPQTbXzEk-06VADNsxSQ6i0OXZeDyh3Jo",
- authDomain: "realtimebusnavigationsystem.firebaseapp.com",
- databaseURL: "https://realtimebusnavigationsystem.firebaseio.com",
- storageBucket: "realtimebusnavigationsystem.appspot.com",
+apiKey: "AIzaSyAPQTbXzEk-06VADNsxSQ6i0OXZeDyh3Jo",
+authDomain: "realtimebusnavigationsystem.firebaseapp.com",
+databaseURL: "https://realtimebusnavigationsystem.firebaseio.com",
+storageBucket: "realtimebusnavigationsystem.appspot.com",
 };
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,52 +34,75 @@ app.use("/assets/fonts",  express.static(__dirname + '/assets/fonts'));
 app.use("/images",  express.static(__dirname + '/images'));
 
 app.get('/', function (req, res) {
-	     res.sendFile('index.html', { root: __dirname  } );   	 
+    res.sendFile('index.html', { root: __dirname  } );
 
 });
 
-
+var nearby = [], latNearby = [], lngNearby = [];
 app.post('/', function(request, response){
+    lat = request.body['coords[latitude]'];
+    lng = request.body['coords[longitude]'];
+    console.log(lat + " " + lng);
+    var get_bus_data;
+    return new Promises(function(resolve,reject){
+        var ref = db.ref("CurrentLocation");
+        ref.once('value', function(result){
+            if(result.val() === undefined)
+            {
+                console.log("undefined data");
+                reject();
+            }
+            else
+            {
+                resolve(result.val());
+            }
+        }).then((result)=>{
+            get_bus_data = result.toJSON();
+            return get_bus_data;
+        }).then((result)=>{
+            response.send(result);
+            var str = JSON.stringify(result);
+            var arr = str.split(/[{'" ,:"'}]+/);
+            for(var i = 1; i< arr.length-1; i++){
+                var id = arr[i++];
+                var Lat = arr[i];
+                var x = parseFloat(arr[i++]);
+                var Lng = arr[i];
+                var y = parseFloat(arr[i]);
 
-	lat = request.body['coords[latitude]'];
-	lng = request.body['coords[longitude]'];
-	console.log(lat + " " + lng);
-	var get_bus_data;
-	return new Promises(function(resolve,reject){
-		var ref = db.ref("CurrentLocation");
-		ref.once('value', function(result){
-			if e(result.val() === undefined)
-			{
-				console.log("undefined data");
-				reject();
-			}
-			else
-			{
-				resolve(result.val());
-			}
-			
-		})
-		.then((result)=>{
-			get_bus_data = result.toJSON();
-			console.log(get_bus_data);
-			return get_bus_data;
-			//console.log(get_bus_data);
-		})
-		.then((result)=>{
-			//console.log(result);
-			response.send(result.CurrentLocation);
-				
-		})
+                console.log(x + " " + y);
 
-		
-	})
+                console.log(getDistance(lat, lng, x, y));
+                if( getDistance(lat, lng, x, y) <= 5){
+                    nearby.push(id);
+                    latNearby.push(Lat);
+                    lngNearby.push(Lng);
+                }
+            }
+            console.log(nearby);
+            console.log(latNearby);
+            console.log(lngNearby);
+        })
+    })
 
 });
 
+https.createServer(options, app).listen(3000, function () {
+  console.log('Started!');
+});
 
-app.listen(3000, function(){
-	console.log('Server Started');
+function getDistance(lat1, lon1, lat2, lon2){
+    var R = 6371;
+    var dLat = deg2rad(lat2-lat1);
+    var dLon = deg2rad(lon2-lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+}
 
-})
-
-
+function deg2rad(deg){
+    return deg * (Math.PI/180)
+}
